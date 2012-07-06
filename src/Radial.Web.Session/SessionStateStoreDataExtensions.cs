2eb9401fd2a7bff645198f96db8f93554e56a128
Rick Zhang <me@zhanghq.net>
2012-07-07 00:12:41 +0800

    Add MemcachedStateStore and test site

---
+++ src/Radial.Web.Session/SessionStateStoreDataExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.SessionState;
+using System.IO;
+using System.Web;
+
+namespace Radial.Web.Session
+{
+    /// <summary>
+    /// SessionStateStoreDataExtensions
+    /// </summary>
+    public static class SessionStateStoreDataExtensions
+    {
+        /// <summary>
+        /// Serializes the specified data.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        /// <returns></returns>
+        public static byte[] Serialize(this SessionStateStoreData data)
+        {
+            if (data == null)
+                return new byte[] { };
+
+            using (MemoryStream ms = new MemoryStream())
+            using (BinaryWriter writer = new BinaryWriter(ms))
+            {
+                ((SessionStateItemCollection)data.Items).Serialize(writer);
+                return ms.ToArray();
+            }
+        }
+    }
+}
---
+++ src/Radial.Web.TestSite.Session/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Radial.Web.TestSite.Session.Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Button ID="btnSet" runat="server" Text="设置Session" 
+            onclick="btnSet_Click" />
+        <asp:Button ID="btnRead" runat="server" Text="读取Session" 
+            onclick="btnRead_Click" />
+        <p>
+            <asp:Literal ID="litValue" runat="server"></asp:Literal>
+        </p>
+    </div>
+    </form>
+</body>
+</html>

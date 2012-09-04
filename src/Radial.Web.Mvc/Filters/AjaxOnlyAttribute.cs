e86af64eb1f9d31591918ed331763e3d9558bdb3
Rick Zhang <me@zhanghq.net>
2012-09-04 17:58:53 +0800

    Add AjaxOnlyAttribute.cs

---
+++ src/Radial.Web.Mvc/Filters/AjaxOnlyAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Radial.Web.Mvc.Filters
+{
+    /// <summary>
+    /// Represents an attribute that is used to identifies only allow Ajax request.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+    public class AjaxOnlyAttribute : ActionMethodSelectorAttribute
+    {
+        /// <summary>
+        ///  Determines whether the action method selection is valid for the specified controller context.
+        /// </summary>
+        /// <param name="controllerContext">The controller context.</param>
+        /// <param name="methodInfo">Information about the action method.</param>
+        /// <returns> true if the action method selection is valid for the specified controller context; otherwise, false.</returns>
+        public override bool IsValidForRequest(ControllerContext controllerContext, System.Reflection.MethodInfo methodInfo)
+        {
+            return controllerContext.RequestContext.HttpContext.Request.IsAjaxRequest();
+        }
+    }
+}
--- src/Radial.Web.TestSite/Controllers/AsyncController.cs
+++ src/Radial.Web.TestSite/Controllers/AsyncController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Radial.Web.Mvc.Filters;
 
 namespace Radial.Web.TestSite.Controllers
 {
@@ -17,6 +18,7 @@ namespace Radial.Web.TestSite.Controllers
         }
 
         [HttpPost]
+        [AjaxOnly]
         public ActionResult Index(string name)
         {
             return Content("Greetings," + name);

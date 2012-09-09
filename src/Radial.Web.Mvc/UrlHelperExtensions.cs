bc51cf4244638fbc6d657be6e1aaf23bab4337a9
Rick Zhang <me@zhanghq.net>
2012-09-09 21:01:04 +0800

    modify Radial.Web.Mvc/UrlHelperExtensions.cs

--- src/Radial.Web.Mvc/UrlHelperExtensions.cs
+++ src/Radial.Web.Mvc/UrlHelperExtensions.cs
@@ -13,50 +13,22 @@ namespace Radial.Web.Mvc
     public static class UrlHelperExtensions
     {
         /// <summary>
-        /// Converts a virtual (relative) image path to an absolute url.
+        /// Resource base url config name. 
         /// </summary>
-        /// <param name="helper">The UrlHelper object.</param>
-        /// <param name="imagePath">The virtual path of the image.</param>
-        /// <returns>The image absolute url.</returns>
-        public static string Image(this UrlHelper helper, string imagePath)
-        {
-            return Resource(helper, imagePath, "ImageBaseUrl");
-        }
-
-        /// <summary>
-        /// Converts a virtual (relative) style sheet path to an absolute url.
-        /// </summary>
-        /// <param name="helper">The UrlHelper object.</param>
-        /// <param name="stylePath">The virtual path of the style sheet.</param>
-        /// <returns>The style sheet absolute url.</returns>
-        public static string Style(this UrlHelper helper, string stylePath)
-        {
-            return Resource(helper, stylePath, "StyleBaseUrl");
-        }
-
-        /// <summary>
-        /// Converts a virtual (relative) script path to an absolute url.
-        /// </summary>
-        /// <param name="helper">The UrlHelper object.</param>
-        /// <param name="scriptPath">The virtual path of the script.</param>
-        /// <returns>The script absolute url.</returns>
-        public static string Script(this UrlHelper helper, string scriptPath)
-        {
-            return Resource(helper, scriptPath, "ScriptBaseUrl");
-        }
+        public const string ResourceBaseUrlConfigName = "ResourceBaseUrl";
 
         /// <summary>
         /// Converts a virtual (relative) path to a resource absolute url.
         /// </summary>
         /// <param name="helper">The UrlHelper object.</param>
         /// <param name="contentPath">The virtual path of the content.</param>
-        /// <param name="baseUrlConfigName">The base url configuration name.</param>
         /// <returns>The resource absolute url.</returns>
-        public static string Resource(this UrlHelper helper, string contentPath, string baseUrlConfigName)
+        public static string Resource(this UrlHelper helper, string contentPath)
         {
-            string baseUrl = ConfigurationManager.AppSettings[baseUrlConfigName];
 
-            if (string.IsNullOrWhiteSpace(baseUrlConfigName) || string.IsNullOrWhiteSpace(baseUrl))
+            string baseUrl = ConfigurationManager.AppSettings[ResourceBaseUrlConfigName];
+
+            if (string.IsNullOrWhiteSpace(ResourceBaseUrlConfigName) || string.IsNullOrWhiteSpace(baseUrl))
                 return helper.Content(contentPath);
 
             return baseUrl.TrimEnd('/', ' ') + "/" + contentPath.TrimStart('~', '/', ' ');

22f32a88d8a47cbb8bd5720e741d60e3eca6caf5
Rick Zhang <me@zhanghq.net>
2012-10-11 18:48:15 +0800

    fix ApplicationPath bug 2

--- src/Radial.Web/HttpKits.cs
+++ src/Radial.Web/HttpKits.cs
@@ -538,12 +538,13 @@ namespace Radial.Web
                 url += ":" + CurrentContext.Request.Url.Port;
             }
 
+            //like '/' or '/app'
             string appPath = CurrentContext.Request.ApplicationPath.Trim('/');
 
             string path = (appPath + '/' + relativeUrl).Trim('/');
 
-            if (relativeUrl.StartsWith(appPath, StringComparison.OrdinalIgnoreCase))
-                path = (appPath + '/' + relativeUrl.Replace(appPath, "").Trim('/')).Trim('/');
+            if (!string.IsNullOrWhiteSpace(appPath) && relativeUrl.StartsWith(appPath, StringComparison.OrdinalIgnoreCase))
+                path = (appPath + '/' + relativeUrl.Replace(appPath + "/", "").Trim('/')).Trim('/');
 
             if (string.IsNullOrWhiteSpace(path))
                 return url;

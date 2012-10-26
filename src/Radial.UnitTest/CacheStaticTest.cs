e9881e5de5160a8a2e311dd4c07345455607760d
Rick Zhang <me@zhanghq.net>
2012-10-26 19:10:00 +0800

    add CacheStaticTest.cs

---
+++ src/Radial.UnitTest/CacheStaticTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Radial.Cache;
+
+namespace Radial.UnitTest
+{
+    [TestFixture]
+    public class CacheStaticTest
+    {
+        class TObj
+        {
+            public string name;
+            public string value;
+        }
+
+        [Test]
+        public void Memcached_IList()
+        {
+            IList<TObj> list = new List<TObj>();
+            list.Add(new TObj { name = "a", value = "c" });
+            list.Add(new TObj { name = "a", value = "c" });
+            list.Add(new TObj { name = "a", value = "c" });
+
+            CacheStatic.Set<IList<TObj>>("a", list, 100);
+
+            IList<TObj> list2 = CacheStatic.Get<IList<TObj>>("a");
+        }
+    }
+}

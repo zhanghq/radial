9b21023283ff18fba271d59bb50ccc4db141330c
Rick Zhang <me@zhanghq.net>
2012-11-29 19:02:16 +0800

    add RangeValue

---
+++ src/Radial/RangeValue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Radial
+{
+    /// <summary>
+    /// Represents a range of value.
+    /// </summary>
+    public struct RangeValue<TValue> where TValue : struct
+    {
+        /// <summary>
+        /// Gets or sets the range minimum value.
+        /// </summary>
+        public TValue? Min { get; set; }
+        /// <summary>
+        /// Gets or sets the range maximum value.
+        /// </summary>
+        public TValue? Max { get; set; }
+    }
+}

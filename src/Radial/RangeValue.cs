6dcb807dc00ad48b1d107c3ffbcf35bcb278cabf
Rick Zhang <me@zhanghq.net>
2012-12-19 19:23:04 +0800

    Modify RangeValue.cs

--- src/Radial/RangeValue.cs
+++ src/Radial/RangeValue.cs
@@ -10,13 +10,27 @@ namespace Radial
     /// </summary>
     public struct RangeValue<TValue> where TValue : struct
     {
+        TValue? _min;
+        TValue? _max;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RangeValue&lt;TValue&gt;"/> class.
+        /// </summary>
+        /// <param name="min">The range minimum value.</param>
+        /// <param name="max">The range maximum value.</param>
+        public RangeValue(TValue? min, TValue? max)
+        {
+            _min = min;
+            _max = max;
+        }
+
         /// <summary>
         /// Gets or sets the range minimum value.
         /// </summary>
-        public TValue? Min { get; set; }
+        public TValue? Min { get { return _min; } set { _min = value; } }
         /// <summary>
         /// Gets or sets the range maximum value.
         /// </summary>
-        public TValue? Max { get; set; }
+        public TValue? Max { get { return _max; } set { _max = value; } }
     }
 }

6250ad0d76b9657b597e1ee74604b404f03b42f1
Rick Zhang <me@zhanghq.net>
2013-01-17 09:06:06 +0800

    fix Hosts tool bug

--- src/Radial.Tools.Hosts/MainForm.Designer.cs
+++ src/Radial.Tools.Hosts/MainForm.Designer.cs
@@ -63,6 +63,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
+            this.button1.TabStop = false;
             this.button1.Text = "保存";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);

9e52802894c27c86cd1ee5dfad55f5a448b1bf1b
Rick Zhang <me@zhanghq.net>
2013-02-27 11:56:23 +0800

    remove ExcelToDataSet

--- src/Radial.UnitTest/ToolkitsTest.cs
+++ src/Radial.UnitTest/ToolkitsTest.cs
@@ -10,28 +10,5 @@ namespace Radial.UnitTest
     [TestFixture]
     public class ToolkitsTest
     {
-        [Test]
-        public void ExcelToDataSet()
-        {
-            Assert.DoesNotThrow(() => Toolkits.ExcelToDataSet("data.xlsx"));
-        }
-
-        [Test]
-        public void ExcelToDataTableIndex()
-        {
-            Assert.DoesNotThrow(() =>
-            {
-                DataTable dt = Toolkits.ExcelToDataTable("data.xlsx", 0);
-            });
-        }
-
-        [Test]
-        public void ExcelToDataTableName()
-        {
-            Assert.DoesNotThrow(() =>
-            {
-                DataTable dt = Toolkits.ExcelToDataTable("data.xlsx", "Sheet1");
-            });
-        }
     }
 }

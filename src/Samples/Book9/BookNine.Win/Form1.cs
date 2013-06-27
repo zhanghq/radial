b51604e81f627a5a9f87945c2c536ecf1790a670
Rick Zhang <me@zhanghq.net>
2013-06-27 15:04:56 +0800

    增加Win示例

---
+++ src/Samples/Book9/BookNine.Win/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BookNine.Application;
+using BookNine.TransferObject;
+using Radial.Boot;
+
+namespace BookNine.Win
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Bootstrapper.Initialize();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Bootstrapper.Start();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Bootstrapper.Stop();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UserModel model = UserService.Create(string.Format("{0}@abc.com", Radial.RandomCode.Create(6).ToLower()), "123456");
+
+            MessageBox.Show(model.Mail);
+        }
+    }
+}

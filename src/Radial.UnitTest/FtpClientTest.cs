82a52fb52d0d2f9da872cd3a9419adf10da430a1
Rick Zhang <me@zhanghq.net>
2013-08-16 17:58:32 +0800

    add Ftp client test

--- src/Radial.UnitTest/FtpClientTest.cs
+++ src/Radial.UnitTest/FtpClientTest.cs
@@ -6,6 +6,7 @@ using NUnit.Framework;
 using System.Net;
 using System.IO;
 using Radial.Net;
+using System.Net.FtpClient;
 
 namespace Radial.UnitTest
 {
@@ -15,17 +16,28 @@ namespace Radial.UnitTest
         [Test]
         public void Demo()
         {
-            FtpWebRequest req = FtpWebRequest.Create("ftp://ftp.adobe.com") as FtpWebRequest;
-            req.Credentials = new NetworkCredential("anonymous", "me@zhanghq.net");
-            req.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
-            req.UsePassive = true;
+            //FtpWebRequest req = FtpWebRequest.Create("ftp://ftp.adobe.com/Acrobat") as FtpWebRequest;
+            //req.Credentials = new NetworkCredential("anonymous", "me@zhanghq.net");
+            //req.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+            //req.UsePassive = true;
 
-            FtpWebResponse resp = req.GetResponse() as FtpWebResponse;
+            //FtpWebResponse resp = req.GetResponse() as FtpWebResponse;
 
-            using (StreamReader sr =new StreamReader(resp.GetResponseStream()))
+            //using (StreamReader sr =new StreamReader(resp.GetResponseStream()))
+            //{
+            //    while (!sr.EndOfStream)
+            //        Console.WriteLine(sr.ReadLine());
+            //}
+
+            using (FtpClient conn = new FtpClient())
             {
-                while (!sr.EndOfStream)
-                    Console.WriteLine(sr.ReadLine());
+                conn.Host = "ftp.adobe.com";
+                conn.Credentials = new NetworkCredential("anonymous", "me@zhanghq.net");
+
+                foreach (FtpListItem item in conn.GetListing("/", FtpListOption.Modify))
+                {
+                    Console.WriteLine("Name: {0} Modify: {1} Size: {2}", item.Name, item.Modified, item.Size);
+                }
             }
         }
     }

66e148d9b6bfdb6d59f29090d1cd42d465460dd3
Rick Zhang <me@zhanghq.net>
2013-08-19 16:24:19 +0800

    modify RedisTest.cs

--- src/Radial.UnitTest/RedisTest.cs
+++ src/Radial.UnitTest/RedisTest.cs
@@ -12,18 +12,31 @@ namespace Radial.UnitTest
     public class RedisTest
     {
         [Test]
-        public void Demo()
+        public void StringsDemo()
         {
-            using (var conn = new RedisConnection("192.168.62.129"))
+            using (var conn = new RedisConnection("subu.cloudapp.net"))
             {
                 conn.Open();
 
                 conn.Strings.Set(0, "foo", "test", 100);
 
-                string val = conn.Strings.GetString(0, "foo").Result;
+                string val =conn.Strings.GetString(0, "foo").Result;
 
                 Console.WriteLine(val);
             }
         }
+
+        [Test]
+        public void HashDemo()
+        {
+            using (var conn = new RedisConnection("subu.cloudapp.net"))
+            {
+                conn.Open();
+                conn.Hashes.Set(0, "user2s", "1", "name");
+                conn.Hashes.Set(0, "user2s", "2", "name1");
+
+                Console.WriteLine(conn.Hashes.GetKeys(0, "user2s").Result.Count());
+            }
+        }            
     }
 }

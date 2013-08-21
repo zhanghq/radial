af95230b6097ae256e8f4273ada01af55c0b4e3c
Rick Zhang <me@zhanghq.net>
2013-08-21 14:05:56 +0800

    add Serialization/JsonTest.cs

---
+++ src/Radial.UnitTest/Serialization/JsonTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radial.UnitTest.Serialization
+{
+    [TestFixture]
+    public class JsonTest
+    {
+        [Test]
+        public void DynamicType()
+        {
+            string jtxt = Radial.Serialization.JsonSerializer.Serialize(new { name = "link", age = 23 });
+
+            dynamic o = Radial.Serialization.JsonSerializer.Deserialize(jtxt);
+
+            Console.WriteLine(o.name);
+        }
+    }
+}

3f82efa4e6d44273bec3ab497285a3827b53b145
zhanghq <ethan@zhanghq.net>
2013-10-23 17:04:49 +0800

    degraded to mvc 4

--- src/Radial.Test.Mvc/Properties/AssemblyInfo.cs
+++ src/Radial.Test.Mvc/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("ed7ef3ca-ceab-4f9b-bf04-538a80240221")]
+[assembly: Guid("2b4edb17-66af-4fd0-af5a-92809b2453ae")]
 
 // 程序集的版本信息由下列四个值组成:
 //
--- src/Radial.Web.Mvc/Properties/AssemblyInfo.cs
+++ src/Radial.Web.Mvc/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("Radial.Web.Mvc")]
-[assembly: AssemblyDescription("Based On Asp.net MVC 5")]
+[assembly: AssemblyDescription("Based On Asp.net MVC 4")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Radial Project")]
 [assembly: AssemblyProduct("Radial.Web.Mvc")]

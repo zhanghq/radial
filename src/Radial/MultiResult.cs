f6c46c31d09a4484a628a9d233935d84f08ab01d
zhanghq <ethan@zhanghq.net>
2014-02-07 12:33:50 +0800

    add src/Radial/MultiResult.cs

---
+++ src/Radial/MultiResult.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radial
+{
+    /// <summary>
+    /// Indicate the multiple result values.
+    /// </summary>
+    /// <typeparam name="T1">The type of result value 1.</typeparam>
+    /// <typeparam name="T2">The type of result value 2.</typeparam>
+    public struct MultiResult<T1, T2>
+    {
+        /// <summary>
+        /// Gets or sets the result value 1.
+        /// </summary>
+        public T1 Value1 { get; set; }
+        /// <summary>
+        /// Gets or sets the result value 2.
+        /// </summary>
+        public T2 Value2 { get; set; }
+    }
+
+    /// <summary>
+    /// Indicate the multiple result values.
+    /// </summary>
+    /// <typeparam name="T1">The type of result value 1.</typeparam>
+    /// <typeparam name="T2">The type of result value 2.</typeparam>
+    /// <typeparam name="T3">The type of result value 3.</typeparam>
+    public struct MultiResult<T1, T2, T3>
+    {
+        /// <summary>
+        /// Gets or sets the result value 1.
+        /// </summary>
+        public T1 Value1 { get; set; }
+        /// <summary>
+        /// Gets or sets the result value 2.
+        /// </summary>
+        public T2 Value2 { get; set; }
+        /// <summary>
+        /// Gets or sets the result value 3.
+        /// </summary>
+        public T3 Value3 { get; set; }
+    }
+
+    /// <summary>
+    /// Indicate the multiple result values.
+    /// </summary>
+    /// <typeparam name="T1">The type of result value 1.</typeparam>
+    /// <typeparam name="T2">The type of result value 2.</typeparam>
+    /// <typeparam name="T3">The type of result value 3.</typeparam>
+    /// <typeparam name="T4">The type of result value 4.</typeparam>
+    public struct MultiResult<T1, T2, T3, T4>
+    {
+        /// <summary>
+        /// Gets or sets the result value 1.
+        /// </summary>
+        public T1 Value1 { get; set; }
+        /// <summary>
+        /// Gets or sets the result value 2.
+        /// </summary>
+        public T2 Value2 { get; set; }
+        /// <summary>
+        /// Gets or sets the result value 3.
+        /// </summary>
+        public T3 Value3 { get; set; }
+        /// <summary>
+        /// Gets or sets the result value 4.
+        /// </summary>
+        public T4 Value4 { get; set; }
+    }
+
+    /// <summary>
+    /// Indicate the multiple result values.
+    /// </summary>
+    /// <typeparam name="T1">The type of result value 1.</typeparam>
+    /// <typeparam name="T2">The type of result value 2.</typeparam>
+    /// <typeparam name="T3">The type of result value 3.</typeparam>
+    /// <typeparam name="T4">The type of result value 4.</typeparam>
+    /// <typeparam name="T5">The type of result value 5.</typeparam>
+    public struct MultiResult<T1, T2, T3, T4, T5>
+    {
+        /// <summary>
+        /// Gets or sets the result value 1.
+        /// </summary>
+        public T1 Value1 { get; set; }
+        /// <summary>
+        /// Gets or sets the result value 2.
+        /// </summary>
+        public T2 Value2 { get; set; }
+        /// <summary>
+        /// Gets or sets the result value 3.
+        /// </summary>
+        public T3 Value3 { get; set; }
+        /// <summary>
+        /// Gets or sets the result value 4.
+        /// </summary>
+        public T4 Value4 { get; set; }
+        /// <summary>
+        /// Gets or sets the result value 5.
+        /// </summary>
+        public T5 Value5 { get; set; }
+    }
+
+    /// <summary>
+    /// Indicate the multiple result values.
+    /// </summary>
+    /// <typeparam name="T1">The type of result value 1.</typeparam>
+    /// <typeparam name="T2">The type of result value 2.</typeparam>
+    /// <typeparam name="T3">The type of result value 3.</typeparam>
+    /// <typeparam name="T4">The type of result value 4.</typeparam>
+    /// <typeparam name="T5">The type of result value 5.</typeparam>
+    /// <typeparam name="T6">The type of result value 6.</typeparam>
+    public struct MultiResult<T1, T2, T3, T4, T5, T6>
+    {
+        /// <summary>
+        /// Gets or sets the result value 1.
+        /// </summary>
+        public T1 Value1 { get; set; }
+        /// <summary>
+        /// Gets or sets the result value 2.
+        /// </summary>
+        public T2 Value2 { get; set; }
+        /// <summary>
+        /// Gets or sets the result value 3.
+        /// </summary>
+        public T3 Value3 { get; set; }
+        /// <summary>
+        /// Gets or sets the result value 4.
+        /// </summary>
+        public T4 Value4 { get; set; }
+        /// <summary>
+        /// Gets or sets the result value 5.
+        /// </summary>
+        public T5 Value5 { get; set; }
+        /// <summary>
+        /// Gets or sets the result value 6.
+        /// </summary>
+        public T6 Value6 { get; set; }
+    }
+
+    /// <summary>
+    /// Indicate the multiple result values.
+    /// </summary>
+    /// <typeparam name="T1">The type of result value 1.</typeparam>
+    /// <typeparam name="T2">The type of result value 2.</typeparam>
+    /// <typeparam name="T3">The type of result value 3.</typeparam>
+    /// <typeparam name="T4">The type of result value 4.</typeparam>
+    /// <typeparam name="T5">The type of result value 5.</typeparam>
+    /// <typeparam name="T6">The type of result value 6.</typeparam>
+    /// <typeparam name="T7">The type of result value 7.</typeparam>
+    public struct MultiResult<T1, T2, T3, T4, T5, T6, T7>
+    {
+        /// <summary>
+        /// Gets or sets the result value 1.
+        /// </summary>
+        public T1 Value1 { get; set; }
+        /// <summary>
+        /// Gets or sets the result value 2.
+        /// </summary>
+        public T2 Value2 { get; set; }
+        /// <summary>
+        /// Gets or sets the result value 3.
+        /// </summary>
+        public T3 Value3 { get; set; }
+        /// <summary>
+        /// Gets or sets the result value 4.
+        /// </summary>
+        public T4 Value4 { get; set; }
+        /// <summary>
+        /// Gets or sets the result value 5.
+        /// </summary>
+        public T5 Value5 { get; set; }
+        /// <summary>
+        /// Gets or sets the result value 6.
+        /// </summary>
+        public T6 Value6 { get; set; }
+        /// <summary>
+        /// Gets or sets the result value 7.
+        /// </summary>
+        public T7 Value7 { get; set; }
+    }
+
+    /// <summary>
+    /// Indicate the multiple result values.
+    /// </summary>
+    /// <typeparam name="T1">The type of result value 1.</typeparam>
+    /// <typeparam name="T2">The type of result value 2.</typeparam>
+    /// <typeparam name="T3">The type of result value 3.</typeparam>
+    /// <typeparam name="T4">The type of result value 4.</typeparam>
+    /// <typeparam name="T5">The type of result value 5.</typeparam>
+    /// <typeparam name="T6">The type of result value 6.</typeparam>
+    /// <typeparam name="T7">The type of result value 7.</typeparam>
+    /// <typeparam name="T8">The type of result value 8.</typeparam>
+    public struct MultiResult<T1, T2, T3, T4, T5, T6, T7, T8>
+    {
+        /// <summary>
+        /// Gets or sets the result value 1.
+        /// </summary>
+        public T1 Value1 { get; set; }
+        /// <summary>
+        /// Gets or sets the result value 2.
+        /// </summary>
+        public T2 Value2 { get; set; }
+        /// <summary>
+        /// Gets or sets the result value 3.
+        /// </summary>
+        public T3 Value3 { get; set; }
+        /// <summary>
+        /// Gets or sets the result value 4.
+        /// </summary>
+        public T4 Value4 { get; set; }
+        /// <summary>
+        /// Gets or sets the result value 5.
+        /// </summary>
+        public T5 Value5 { get; set; }
+        /// <summary>
+        /// Gets or sets the result value 6.
+        /// </summary>
+        public T6 Value6 { get; set; }
+        /// <summary>
+        /// Gets or sets the result value 7.
+        /// </summary>
+        public T7 Value7 { get; set; }
+        /// <summary>
+        /// Gets or sets the result value 8.
+        /// </summary>
+        public T8 Value8 { get; set; }
+    }
+}

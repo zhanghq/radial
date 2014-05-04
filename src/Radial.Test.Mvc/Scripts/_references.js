519e6264ba0c34e6573cde337e13b55ce2a05c94
zhanghq <ethan@zhanghq.net>
2014-05-04 10:40:42 +0800

    upgrade dlls

--- src/Radial.Test.Mvc/Scripts/_references.js
+++ src/Radial.Test.Mvc/Scripts/_references.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="modernizr-2.7.1.js" />
+﻿/// <reference path="modernizr-2.7.2.js" />
 /// <reference path="jquery-ui-1.10.4.js" />
 /// <reference path="jquery-2.1.0.js" />
 /// <reference path="jquery.validate.js" />

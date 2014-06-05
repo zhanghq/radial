f3ce9adf7854a4a8cab73f7e29afd6b79dd6b3aa
zhanghq <ethan@zhanghq.net>
2014-06-05 18:06:06 +0800

    add modules

---
+++ modules/Radial.Modules.UserUnit/Application/IUserService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Radial.Modules.UserUnit.Models;
+
+//For Demonstrate Only
+namespace Radial.Modules.UserUnit.Application
+{
+    /// <summary>
+    /// IUserService
+    /// </summary>
+    public interface IUserService
+    {
+        /// <summary>
+        /// Creates the specified name.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="email">The email.</param>
+        /// <returns></returns>
+        User Create(string name, string email);
+
+        /// <summary>
+        /// Gets all.
+        /// </summary>
+        /// <returns></returns>
+        IList<User> GetAll();
+    }
+}
---
+++ modules/Radial.Modules.UserUnit/Models/ModelBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Radial;
+
+namespace Radial.Modules.UserUnit.Models
+{
+    /// <summary>
+    /// ModelBase
+    /// </summary>
+    public abstract class ModelBase
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the version.
+        /// </summary>
+        public int Version { get; set; }
+    }
+}

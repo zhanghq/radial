fa7cb979f9d5de31077bae845c1b7f084a62b552
zhanghq <ethan@zhanghq.net>
2014-07-20 23:36:22 +0800

    Add MultiThreadTest

---
+++ src/Radial.UnitTest/Persist/Nhs/MultiThreadTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using Radial.Persist;
+using Radial.Persist.Nhs;
+using Radial.UnitTest.Persist.Nhs.Domain;
+using Radial.UnitTest.Persist.Nhs.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Radial.UnitTest.Persist.Nhs
+{
+    [TestFixture]
+    public class MultiThreadTest
+    {
+        [Test]
+        public void Insert()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Thread t = new Thread(InsertThread);
+                t.IsBackground = true;
+                t.Start();
+            }
+
+            Thread.CurrentThread.Join(10 * 1000);
+        }
+
+        private void InsertThread()
+        {
+            using (IUnitOfWork uow = new NhUnitOfWork())
+            {
+                User u = new User
+                {
+                    Id = RandomCode.NewInstance.Next(10000, int.MaxValue),
+                    Name = Thread.CurrentThread.ManagedThreadId + "-mt-text2"
+                };
+                uow.RegisterNew<User>(u);
+                uow.Commit();
+            }
+        }
+    }
+}

36174c0c9d312f6e6851aa64a078145c35093731
zhanghq <ethan@zhanghq.net>
2014-07-24 13:39:41 +0800

    update TimingIdGenerator

---
+++ src/Radial.UnitTest/Persist/Nhs/IdentifierTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Radial.Persist;
+using Radial.Persist.Nhs;
+using Radial.UnitTest.Persist.Nhs.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radial.UnitTest.Persist.Nhs
+{
+    [TestFixture]
+    public class IdentifierTest
+    {
+        [Test]
+        public void Create()
+        {
+            using (IUnitOfWork uow = new NhUnitOfWork())
+            {
+                Team t = new Team();
+                //当Id赋值时，则从赋值，若没赋值，则自动产生Id
+                t.Id = Guid.NewGuid().ToString("n").Substring(0, 15).ToLower();
+                t.Name = "adfadf";
+                uow.RegisterNew(t);
+                uow.Commit();
+            }
+        }
+    }
+}
--- src/Radial/Persist/Nhs/TimingIdGenerator.cs
+++ src/Radial/Persist/Nhs/TimingIdGenerator.cs
@@ -22,7 +22,25 @@ namespace Radial.Persist.Nhs
         /// </returns>
         public object Generate(NHibernate.Engine.ISessionImplementor session, object obj)
         {
-            return TimingSeq.Next();
+            Checker.Parameter(obj != null, "object can not be null");
+
+            Type objType = obj.GetType();
+
+            var metadata = session.Factory.GetClassMetadata(objType);
+
+            Checker.Requires(metadata.HasIdentifierProperty, "{0} does not has identifier property", objType.FullName);
+
+            var p = objType.GetProperty(metadata.IdentifierPropertyName);
+
+            Checker.Requires(p != null, "can not find object identifier property {0} in {1}", metadata.IdentifierPropertyName, objType.FullName);
+            Checker.Requires(p.PropertyType == typeof(string), "not support identifier type {0}, please use System.String instead", objType.FullName);
+
+            string objIdVal = p.GetValue(obj) as string;
+
+            if (string.IsNullOrWhiteSpace(objIdVal))
+                return TimingSeq.Next();
+
+            return objIdVal;
         }
     }
 }

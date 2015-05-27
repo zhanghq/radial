55bb77d1dec0e2cc1d2d7f700092693bbe65d98a
zhanghq <ethan@zhanghq.net>
2015-05-27 14:16:24 +0800

    add Quartz test

--- src/Radial.Test.Mvc/BootTask.cs
+++ src/Radial.Test.Mvc/BootTask.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Practices.Unity;
+using Quartz;
+using Quartz.Impl;
 using Radial.Boot;
 using Radial.Param;
 using Radial.Persist.Nhs.Param;
@@ -12,17 +14,29 @@ namespace Radial.Test.Mvc
 {
     public class BootTask : IBootTask
     {
+        IScheduler scheduler;
+        IJobDetail testJob;
+        ITrigger testTrigger;
+
         public void Initialize()
         {
             Components.Container.RegisterType<IParam, KvParam>();
+
+            scheduler = (new StdSchedulerFactory()).GetScheduler();
+            testJob = JobBuilder.Create<TestJob>().WithIdentity("testJob").Build();
+            testTrigger = TriggerBuilder.Create().WithIdentity("testTrigger").StartNow().WithSimpleSchedule(x => x.WithIntervalInMinutes(2).RepeatForever()).Build();
+            scheduler.ScheduleJob(testJob, testTrigger);
+            scheduler.Start();
         }
 
         public void Start()
         {
+            
         }
 
         public void Stop()
         {
+            //scheduler.Shutdown();
         }
     }
 }
---
+++ src/Radial.Test.Mvc/TestJob.cs
@@ -0,0 +1,16 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Radial.Test.Mvc
+{
+    public class TestJob : IJob 
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            Logger.Default.Info("Test Job, {0}", DateTime.Now);
+        }
+    }
+}

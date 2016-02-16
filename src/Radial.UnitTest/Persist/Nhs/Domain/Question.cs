a754799feabd800d32a81edcd3daaec2dc9f6043
zhanghq <ethan@zhanghq.net>
2016-02-16 18:33:14 +0800

    v2.2

---
+++ src/Radial.UnitTest/Persist/Nhs/Domain/Question.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radial.UnitTest.Persist.Nhs.Domain
+{
+    /// <summary>
+    /// Question
+    /// </summary>
+    public class Question
+    {
+        public string Id { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Phase { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+}

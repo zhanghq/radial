f958fe409ce53cf7a0820371c6f0e2c9250f1068
zhanghq <ethan@zhanghq.net>
2016-02-16 23:54:32 +0800

    add and remove files

--- src/Radial.UnitTest/Persist/Domain/Question.cs
+++ src/Radial.UnitTest/Persist/Domain/Question.cs
@@ -4,17 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Radial.UnitTest.Persist.Nhs.Domain
+namespace Radial.UnitTest.Persist.Domain
 {
-    /// <summary>
-    /// Question
-    /// </summary>
-    public class Question
+
+    public class Question : ModelBase
     {
-        public string Id { get; set; }
-
         public string Subject { get; set; }
-
         public string Phase { get; set; }
         public DateTime CreateTime { get; set; }
     }

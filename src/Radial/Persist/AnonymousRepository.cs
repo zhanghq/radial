5c52311aa3aae64f417e8f43d307ad2746d6bc9d
zhanghq <ethan@zhanghq.net>
2016-07-03 22:05:21 +0800

    fix

--- src/Radial/Persist/AnonymousRepository.cs
+++ src/Radial/Persist/AnonymousRepository.cs
@@ -55,7 +55,7 @@ namespace Radial.Persist
 
                 if (tobjType != null)
                 {
-                    string nsName = Radial.RandomCode.GetIdentityKey();
+                    string nsName ="N"+Guid.NewGuid().ToString("N").ToUpper();
                     string tobjTypeName = tobjType.FullName;
                     string repositoryInterfaceName = repoType.FullName;
                     string repositoryClassName = Toolkits.FirstCharUpperCase(repoType.Name.TrimStart('I'));

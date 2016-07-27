b9218baafe3a922deabbeac771c1593f54911a20
zhanghq <ethan@zhanghq.net>
2016-07-27 15:49:32 +0800

    change code

---
+++ src/Radial.UnitTest/Persist/SearchResultTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Radial.Persist;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radial.UnitTest.Persist
+{
+    [TestFixture]
+    public class SearchResultTest
+    {
+        [Test]
+        public void Serialization()
+        {
+            var srt = new SearchResult<IList<int>>();
+            srt.Data = new List<int>();
+            srt.Data.Add(100);
+            srt.ObjectTotal = 100;
+            srt.PageSize = 15;
+
+            string json = Radial.Serialization.JsonSerializer.Serialize(srt);
+
+            var srt1 = Radial.Serialization.JsonSerializer.Deserialize<SearchResult<IList<int>>>(json);
+
+            Assert.AreEqual(srt.PageTotal, srt1.PageTotal);
+        }
+    }
+}
--- src/Radial/Persist/SearchResult.cs
+++ src/Radial/Persist/SearchResult.cs
@@ -5,13 +5,15 @@ using System.Text;
 
 namespace Radial.Persist
 {
-
     /// <summary>
-    /// Search result..
+    /// Search result.
     /// </summary>
     /// <typeparam name="TData">The type of the data.</typeparam>
     public class SearchResult<TData> where TData : class
     {
+        int? _objectTotal;
+        int? _pageSize;
+        int? _pageTotal;
 
         /// <summary>
         /// Gets or sets the data.
@@ -21,11 +23,55 @@ namespace Radial.Persist
         /// <summary>
         /// Gets or sets the object total if pageable.
         /// </summary>
-        public int? ObjectTotal { get; set; }
+        public int? ObjectTotal
+        {
+            get
+            {
+                return _objectTotal;
+            }
+            set
+            {
+                _objectTotal = value;
+                SetPageTotal();
+            }
+        }
 
         /// <summary>
-        /// Gets or sets the page total if pageable.
+        /// Gets or sets the size of the page if pageable.
         /// </summary>
-        public int? PageTotal { get; set; }
+        public int? PageSize
+        {
+            get
+            {
+                return _pageSize;
+            }
+            set
+            {
+                _pageSize = value;
+                SetPageTotal();
+            }
+        }
+
+        /// <summary>
+        /// Gets the page total if pageable.
+        /// </summary>
+        public int? PageTotal
+        {
+            get
+            {
+                return _pageTotal;
+            }
+        }
+
+        /// <summary>
+        /// Set page total.
+        /// </summary>
+        private void SetPageTotal()
+        {
+            _pageTotal = null;
+
+            if (_objectTotal.HasValue && _pageSize.HasValue)
+                _pageTotal = (int)Math.Ceiling((double)_objectTotal.Value / _pageSize.Value);
+        }
     }
 }
